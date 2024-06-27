3164e024f230a04046341761024e6aa8480cfd7b
Komforkin <forkin.kir@gmail.com>
2024-06-27 14:27:18 +0700

    Modify a solution for Task1

--- Homework1/Task1.c
+++ Homework1/Task1.c
@@ -4,13 +4,13 @@ int main(void)
 {
     unsigned int a = 0;
     printf("Введите целое положительное число: ");
-    if (scanf("%u", &a) != 1)
+    if (scanf("%u", &a) == 0)
     {
         printf("Введено не число!\n");
         return 1;
     }
-    printf("%u", a);
-    for (int i = 0; i < 8*sizeof(a); i++)
+    unsigned int i = 0;
+    for (i = 0; i < 8*sizeof(a); i++)
     {
         if ((a & 0x80000000) == 0)
             printf("%c", '0');
