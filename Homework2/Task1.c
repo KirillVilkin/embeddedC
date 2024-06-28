8fac08d6d9946bcf5c409692ca2c91a6a4317459
Komforkin <forkin.kir@gmail.com>
2024-06-28 18:37:42 +0700

    Add a solution Task1 Homework2

---
+++ Homework2/Task1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define N 4
+
+int main(void)
+{
+    int A[N][N];
+    unsigned int i = 0;
+    unsigned int j = 0;
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            A[i][j] = i * N + j + 1;
+            printf("%d ", A[i][j]);
+        }
+        printf("\n");
+    }
+}
