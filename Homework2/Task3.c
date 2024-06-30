1c4fc6a2536f4d70612b50956e98d61e26b555d3
Komforkin <forkin.kir@gmail.com>
2024-06-30 23:48:55 +0700

    Homework2|Task3: Убрал лишнюю строку

--- Homework2/Task3.c
+++ Homework2/Task3.c
@@ -7,7 +7,7 @@ int main(void)
     int A[N][N];
     unsigned int i = 0;
     unsigned int j = 0;
-    for (i = 0; i < N; i++)
+    for (; i < N; i++)
         for (j = 0; j < N; j++)
             if (i + j < N - 1)
                 A[i][j] = 0;
